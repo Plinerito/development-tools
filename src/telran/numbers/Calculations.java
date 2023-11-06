9b3444a4a4c3356372f6bac0bd7cbd5820c37e5a
Plinerito <plinerzh@gmail.com>
2023-11-06 16:29:20 +0200

    Initial code

---
+++ src/telran/numbers/Calculations.java
@@ -0,0 +1,16 @@
+package telran.numbers;
+
+public class Calculations {
+     public static double sum(double op1, double op2) {
+    	 return op1 + op2;
+     }
+     public static double subtract(double op1, double op2) {
+    	 return op1 - op2;
+     }
+     public static double multiplay(double op1, double op2) {
+    	 return op1 + op2;
+     }
+     public static double divede(double op1, double op2) {
+    	 return op1 / op2;
+     }
+}
