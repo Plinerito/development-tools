29666bbbffe2e7d799f4e7142db552a083ccef79
Plinerito <plinerzh@gmail.com>
2023-11-06 17:27:55 +0200

    Initial code

--- src/telran/numbers/Calculations.java
+++ src/telran/numbers/Calculations.java
@@ -4,13 +4,13 @@ public class Calculations {
      public static double sum(double op1, double op2) {
     	 return op1 + op2;
      }
-     public static double subtract(double op1, double op2) {
+     public static double substract(double op1, double op2) {
     	 return op1 - op2;
      }
-     public static double multiplay(double op1, double op2) {
-    	 return op1 + op2;
+     public static double multiply(double op1, double op2) {
+    	 return op1 * op2;
      }
-     public static double divede(double op1, double op2) {
+     public static double divide(double op1, double op2) {
     	 return op1 / op2;
      }
 }
